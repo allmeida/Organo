033052542ae05a6da737f72fd05b9af96d39e20f
francinildoalmeida <francinildoalmeida@lampp-it.com.br>
2022-09-06 18:47:46 -0400

    Escutando eventos e Validando formulario

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -15,14 +15,18 @@ const Formulario = () => {
     'Inovação e Gestão'
   ]
 
+  const aoSalvar = () => {
+    console.log('Formulario foi submetido!')
+  }
+
   return (
     <section className='formulario'>
-      <form>
+      <form onSubmit={aoSalvar}>
         <h1>Preencha os dados para criar o card do colaborador.</h1>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
         <Botao>
           Criar Card
         </Botao>
