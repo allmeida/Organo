b4b776c5c990e39d1708d39eccf68e9ff8e2a9f1
francinildoalmeida <francinildoalmeida@lampp-it.com.br>
2022-09-07 18:32:55 -0400

    Criando card de colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src="https://github.com/allmeida.png" alt="Francinildo Almeida" />
+      </div>
+      <div className='rodape'>
+        <h4>francinildo Almeida</h4>
+        <h5>Programador</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador;
