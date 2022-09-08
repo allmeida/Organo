a376c8726c214f2cc32d6903f00f7a1d1b2d8102
francinildoalmeida <francinildoalmeida@lampp-it.com.br>
2022-09-08 13:01:26 -0400

    Mapeando os colaboradores

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css';
 
-const Colaborador = () => {
+const Colaborador = ({ nome, cargo, imagem }) => {
   return (
     <div className='colaborador'>
       <div className='cabecalho'>
-        <img src="https://github.com/allmeida.png" alt="Francinildo Almeida" />
+        <img src={imagem} alt={nome} />
       </div>
       <div className='rodape'>
-        <h4>francinildo Almeida</h4>
-        <h5>Programador</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   )
